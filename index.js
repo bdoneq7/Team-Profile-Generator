e3136e4ca9a7dec849e61e0294782e4f8c024527
Brian Done <briguy2078@gmail.com>
2021-12-27 17:42:56 -0500

    updated index questions, employee and manager function array, and writeFile

--- index.js
+++ index.js
@@ -1,153 +1,216 @@
-// Generate HTML
-const renderHTML = require('./src/renderHtml');
+// Generate HTM File
+const renderHtml = require('./src/renderHtml');
 
-// Include File System and Inquirer
+// Include Node File System and Npm Inquirer
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// Include Classes
+// Include Classes from Library
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-// Array
-const teamMembers = [];
+// Create Employee array
+const employeeArray = []; 
+
+// Ask Manager Questions via Inquirer
+const managerSetup = () => {
+
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name', // key and user input
+            message: 'Who is the Team Manager Name?', // Manager Name
+            validate: teamManagerInput => {
+                if (teamManagerInput) {
+                    return true;
+                } else {
+                    console.log ("Error: Please enter the Team Manager Name");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the Manager ID", // Manager ID
+            validate: managerIdInput => {
+                if (managerIdInput) {
+                    return true;
+                } else {
+                    console.log('Error: Please enter the Manager ID');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the Manager Email Address", // Manager Email
+            validate: managerEmailInput => {
+                if (managerEmailInput) {
+                    return true;
+                } else {
+                    console.log('Error: Please enter a Manager Email Address');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Please enter the Manager Office Number", // Manager Office #
+            validate: officeNumberInput => {
+                if (officeNumberInput) {
+                    return true;
+                } else {
+                    console.log('Error: Please enter the Manager office');
+                    return false;
+                }
+            }
+        }
+    ])
+
+    .then(managerInfo => {
+        const  { name, id, email, officeNumber } = managerInfo; 
+        const manager = new Manager (name, id, email, officeNumber);
+
+        employeeArray.push(manager); 
+        console.log(manager); 
+    })
+};
+
+const employeeSetup = () => {
+
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Please enter the Employee Role", // Employee Role
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "Please enter the Employee Name", // Employee Name
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Error: Please enter the Employee Name");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the Employee ID", // Employee ID
+            validate: employeeIDInput => {
+                if (employeeIDInput) {
+                    return true;
+                } else {
+                    console.log ("Error: Please enter the Employee ID");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the Employee Email Address", // Employee Email
+            validate: employeeEmailInput => {
+                if (employeeEmailInput) {
+                    return true;
+                } else {
+                    console.log ("Error: Please enter the Employee Email Address");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Please enter the Employee Github Username", // Employee Github
+            when: (input) => input.role === "Engineer",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ("Error: Please enter the Employee Github Username")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Please enter the Intern School", // Intern School
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Error: Please enter the Intern School")
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'addMoreEmployees',
+            message: 'Do you want to add more Employees?', // Add more Employees?
+            default: false
+        }
+    ])
+    .then(employeeInfo => {
+
+        let { name, id, email, role, github, school, addMoreEmployees } = employeeInfo; 
+        let employee; 
+
+        if (role === "Engineer") {
+            employee = new Engineer (name, id, email, github);
+
+            console.log(employee);
+
+        } else if (role === "Intern") {
+            employee = new Intern (name, id, email, school);
+
+            console.log(employee);
+        }
+
+        employeeArray.push(employee); 
+
+        if (addMoreEmployees) {
+            return employeeSetup(employeeArray); 
+        } else {
+            return employeeArray;
+        }
+    })
+
+};
 
 
-// Ask User Questions
-const promptUser = () => {
-    return inquirer.prompt([ // Promise
-            {
-                type: 'input',
-                name: 'teamManager', // key and user input
-                message: 'Who is the Team Manager?',
-                validate: teamManagerInput => {
-                    if (teamManagerInput) {
-                        return true;
-                    } else {
-                        console.log('Error: Please enter the Team Manager');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'managerId',
-                message: 'Please enter the Mananger ID',
-                validate: managerIdInput => {
-                    if (managerIdInput) {
-                        return true;
-                    } else {
-                        console.log('Error: Please enter the Manager ID');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'managerEmail',
-                message: 'Please enter the Manager Email Address',
-                validate: managerEmailInput => {
-                    if (managerEmailInput) {
-                        return true;
-                    } else {
-                        console.log('Error: Please enter the Manager Email Address');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'officeNumber',
-                message: 'Please enter the Manager Office Number',
-                validate: officeNumberInput => {
-                    if (officeNumberInput) {
-                        return true;
-                    } else {
-                        console.log('Error: Please enter the Manager Office Number');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'checkbox',
-                name: 'employeeType',
-                message: 'Add Employee: Please enter the Employee Role',
-                choices: ['Engineer', 'Intern'],
-                    // If Engineer Selected, Prompt to Enter Engineer's Name, ID, Email, and Github Username
-                    // Then taken back to the Menu
+// function to generate HTML page file using file system 
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the profile has been created 
+        } else {
+            console.log("Html File Created!")
+        }
+    })
+}; 
 
-                    // If Intern Selected, Prompt to Enter Intern's Name, ID, Email, and School
-                    // Then taken back to the Menu
-            },
-            {
-                type: 'input',
-                name: 'employeeName',
-                message: 'Please enter the Employee Name',
-                validate: employeeNameInput => {
-                    if (employeeNameInput) {
-                        return true;
-                    } else {
-                        console.log('Error: Please enter the Employee Name');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'employeeID',
-                message: 'Please enter the Employee ID',
-                validate: employeeIDInput => {
-                    if (employeeIDInput) {
-                        return true;
-                    } else {
-                        console.log('Error: Please enter the Employee ID');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'employeeEmail',
-                message: 'Please enter the Employee Email',
-                validate: employeeEmailInput => {
-                    if (employeeEmailInput) {
-                        return true;
-                    } else {
-                        console.log('Error: Please enter the Employee Email');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'employeeGithub',
-                message: 'Please enter the Employee Github Username',
-                validate: employeeGithubInput => {
-                    if (employeeGithubInput) {
-                        return true;
-                    } else {
-                        console.log('Error: Please enter the Employee Github Username');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'moreEmployees',
-                message: 'Do you want to add more Employees? (Y/n)',
-                validate: moreEmployeesInput => {
-                    if (moreEmployeesInput) {
-                        return true;
-                    } else {
-                        console.log('Error: Yes or No?');
-                        return false;
-                    }
-                }
-            },
-            
-
-        ]);
-    };
-
-    promptUser();
+managerSetup()
+  .then(employeeSetup)
+  .then(employeeArray => {
+    return renderHtml(employeeArray);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .catch(err => {
+ console.log(err);
+  });
